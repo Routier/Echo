2f930612b65fdd3f5d1550233b12b68dbdeadce0
num13 <num13-ne@mail.ru>
2024-05-29 19:55:18 +0300

    Valid Fail

--- src/test/java/EchoTest.java
+++ src/test/java/EchoTest.java
@@ -18,7 +18,7 @@ public class EchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("something"))
         ;
     }
 }
